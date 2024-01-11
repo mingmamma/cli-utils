8e876e836e971ffc0808075da5695f69935a2500
Ming Ma <18562714+mingmamma@users.noreply.github.com>
2024-01-11 13:45:02 +1100

    some addition

--- src/lib.rs
+++ src/lib.rs
@@ -5,34 +5,82 @@
 //! use cli_utils::read_stdin;
 //! let input = read_stdin();
 //! ```
-//! # Panics:
-//! The `read_stdin` function will panic if it fails to read a line with a message "Failed to read input line".
 
-use std::io::{self, BufRead};
+use std::{io::{self, BufRead}, error::Error};
+use clap::Parser;
 
 pub mod config;
 pub mod colors;
 
-/// This function reads a line from stdin and returns it as a String.
-/// It will panic if it fails to read a line with a message "Failed to read input line".
+#[derive(Parser)]
+pub struct Cli {
+    #[arg(short('f'))]
+    field: usize,
+    #[arg(short('d'))]
+    delimiter: String,
+    #[arg(short('c'))]
+    color: String,
+    #[arg(long)]
+    debug: bool,
+}
+
+/// This function reads a line from stdin and returns it as a Result<String, std::io::Error>.
 /// # Examples:
 /// ```
 /// use cli_utils::read_stdin;
-/// let input = read_stdin();
+/// let input = read_stdin().unwrap();
 /// ```
-pub fn read_stdin() -> String {
+pub fn read_stdin() -> Result<String, std::io::Error> {
     let stdin = std::io::stdin();
     let mut reader = io::BufReader::new(stdin.lock());
     _read_stdin(&mut reader)
 }
 
-fn _read_stdin<R: BufRead>(reader: &mut R) -> String {
+fn _read_stdin<R: BufRead>(reader: &mut R) -> Result<String, std::io::Error> {
     let mut line = String::new();
     reader
-        .read_line(&mut line)
-        .expect("Failed to read input line");
+        .read_line(&mut line)?;
+        //.expect("Failed to read input line");
     
-    line.trim().to_string()
+    Ok(line.trim().to_string())
+}
+
+pub fn split(s: String, cli: &Cli) -> Result<String, &str> {
+    let s_parts: Vec<&str> = s.split(&cli.delimiter).collect();
+    match s_parts.get(cli.field) {
+        Some(&s) => Ok(s.to_string()),
+        None => Err("No field found at index")
+    }
+}
+
+pub fn display(s: String, cli: &Cli) {
+    let color = match cli.color.as_str() {
+        "r" => colors::Color::Red,
+        "b" => colors::Color::Blue,
+        "y" => colors::Color::Yellow,
+        "g" => colors::Color::Green,
+        &_ => panic!("Invalid color option")
+    };
+
+    let mut color_struct = colors::ColorString {
+        color: color,
+        string: s,
+        colorised: String::new(),
+    };
+
+    color_struct.paint();
+
+    println!("{}", color_struct.colorised)
+}
+
+pub fn run(cli: &Cli) -> Result<(), Box<dyn Error>> {
+    let input_s = read_stdin()?;
+
+    let output_s = split(input_s, cli)?;
+
+    display(output_s, cli);
+
+    Ok(())
 }
 
 #[cfg(test)]
@@ -46,7 +94,7 @@ mod tests {
         let expected_output = "Hello World";
         let mut cursor = Cursor::new(input);
         
-        let output = _read_stdin(&mut cursor);
+        let output = _read_stdin(&mut cursor).unwrap();
         assert_eq!(output, expected_output);
     }
 
@@ -55,7 +103,7 @@ mod tests {
         let input = "\n";
         let expected_output = "";
         let mut reader = Cursor::new(input);
-        let output = _read_stdin(&mut reader);
+        let output = _read_stdin(&mut reader).unwrap();
         assert_eq!(output, expected_output);
     }
 }
---
+++ src/main.rs
@@ -0,0 +1,14 @@
+use std::process;
+use clap::Parser;
+use cli_utils::{Cli, run};
+
+
+
+fn main() {
+    let commands = Cli::parse();
+    
+    if let Err(err) = run(&commands) {
+        eprintln!("Application running error: {err}");
+        process::exit(1);
+    }
+}
